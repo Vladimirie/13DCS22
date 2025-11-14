f470ba55bf5c4a85cc3ba1e515ee5adebe28b555
Sound Crafter <konor.fuglovics@gmail.com>
2025-11-14 13:30:15 +0100

    akna

---
+++ Fuglovics Konor/Minesweeper/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Minesweeper
+{
+	internal class MineField
+	{
+		public int PosX
+		{
+			get;
+			set;
+		}
+		public int PosY
+		{
+			get;
+			set;
+		}
+		public MineField(int posx, int posy)
+		{
+			PosX=posx;
+			PosY=posy;
+		}
+	}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
