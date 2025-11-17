58f33078205b70953cd6803641ccb09635f75b75
Szöllősy Marcell <marcell.szollosy@katonaj-mkszig.hu>
2025-11-17 09:52:16 +0100

    AJAX

---
+++ Szollosy Marcell/Frontend BT/AJAX/ajax_projeckt/get_countries.php
@@ -0,0 +1,8 @@
+<?php require 'config.php'; 
+header('Content-Type: application/json'); 
+ 
+$stmt = $pdo->query("SELECT id, name FROM countries LIMIT 5"); 
+$countries = $stmt->fetchAll(); 
+ 
+echo json_encode($countries); 
+?> 
