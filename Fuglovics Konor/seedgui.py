0c68e018ee7465e012ca2edf1140992e848435b2
SoundCrafterYT <konor.fuglovics@gmail.com>
2025-12-01 21:13:50 +0100

    py

--- Fuglovics Konor/seedgui.py
+++ Fuglovics Konor/seedgui.py
@@ -1,6 +1,8 @@
 from tkinter import *
+from tkinter.ttk import *
 from tkinter import Menu
 from tkinter import messagebox
+from threading import Timer
 import random
 import time
 root = Tk()
@@ -8,14 +10,18 @@ root.geometry("350x140")
 def ask():
 	msgbox = messagebox.askyesno("Warning!", "Are you sure you want to quit?")
 	if(msgbox == True):
-		root.destroy()
+		t.cancel()
+		if(t2.is_alive()==True):
+			t2.cancel()
+		else:
+			root.destroy()
 def about():
 	aboutdialog = Toplevel(root)
 	aboutdialog.geometry("300x250")
 	aboutdialog.resizable(False, False)
 	aboutdialog.title("About Seed Generator")
 	aboutdialog.transient()
-	Label(aboutdialog, text="Minecraft Seed Generator Version 0.1").grid(row=0, column=0, sticky="ew")
+	Label(aboutdialog, text="Minecraft Seed Generator Version 0.1").grid(row=0, column=0)
 	Label(aboutdialog, text="Created by Sound Crafter").grid(row=1, column=0)
 	Button(aboutdialog, text="Close", width=10, command=aboutdialog.destroy).grid(row=2, column=0)
 	aboutdialog.columnconfigure(0, weight=1)
@@ -27,10 +33,25 @@ def onclick():
 	seednum = rnd
 	lbl.config(textvariable=rng)
 def copyclip():
+	global t2
+	def change():
+		copy.config(text="Generate a seed.", style="Copy.TLabel")
+	if(lbl.cget("textvariable")==0):
+		t2 = Timer(2.0,change)
+		copy.config(text="No seed copied!", style="Error.TLabel")
+		t2.start()
+	global t
+	t = Timer(5.0,change)
 	root.clipboard_clear()
 	root.clipboard_append(f"{rng.get()}")
 	root.update()
-	copy.config(text="Seed copied!", fg='#0f0')
+	copy.config(text="Seed copied!", style="SeedCopied.TLabel")
+	t.start()
+style = Style()
+style.configure("Copy.TLabel", sticky="ew", padding=(0,10))
+style.configure("SeedCopied.TLabel", sticky="ew", padding=(0,10), foreground='#0f0')
+style.configure("Error.TLabel", sticky="ew", padding=(0,10), foreground='#f00')
+style.configure("Lbl.TLabel", sticky="ew", width=40)
 menubar = Menu(root)
 root.config(menu=menubar)
 about_menu = Menu(menubar, tearoff=False)
@@ -40,11 +61,12 @@ root.resizable(False, False)
 rng = IntVar()
 root.title("Minecraft Seed Generator")
 copy = Label(root)
-copy.config(pady=10, text="Generate a seed.")
-copy.grid(row=1, column=0, columnspan=2, sticky="ew")
+copy.config(text="Generate a seed.", style="Copy.TLabel")
+print(Style().lookup('Copy.TLabel','foreground'))
+copy.grid(row=1, column=0, columnspan=2)
 lbl = Label(root)
-lbl.config(relief="sunken")
-lbl.grid(row=2, column=0, columnspan=2, sticky="ew", padx=50)
+lbl.config(relief="sunken", style="Lbl.TLabel", anchor="center")
+lbl.grid(row=2, column=0, columnspan=2)
 Button(root, text="Generate", command=onclick, width=10).grid(row=3, column=0, sticky="ew", padx=50, pady=10)
 Button(root, text="Copy", command=copyclip, width=10).grid(row=3, column=1, sticky="ew", padx=50, pady=10)
 root.columnconfigure(0, weight=1)
