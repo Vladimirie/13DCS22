b9063a0b0b7a4cfc4972830ec9e148c191f3376f
Gabi455 <90388665+Gabi4555@users.noreply.github.com>
2026-01-06 10:47:24 +0100

    I shoud sabotas a nuclear reactor

---
+++ Újvárosi Katona Gábriel/sql/1/feladat6.sql
@@ -0,0 +1,2 @@
+SELECT felhasznalo.veznev, felhasznalo.utonev, uzenet.tartalom, uzenet.kuldido FROM felhasznalo, uzenet ,hirfolyam WHERE felhasznalo.id=uzenet.f_id AND uzenet.tartalom
+LIKE Concat("%", hirfolyam.megnevezes ,"%")
