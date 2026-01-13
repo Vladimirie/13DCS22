44d8be20ca8c650917d9fd5635237c9970fb8ff8
Uivarosi Gabriel <gabriel.uivarosi@katonaj-mkszig.hu>
2026-01-13 13:37:33 +0100

    fuck

---
+++ Újvárosi Katona Gábriel/c#/Zulu/Zulu/Tanar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zulu
+{
+    internal class Tanar 
+    {
+
+        public string Id { get; set; }
+        public string Nev { get; set; }
+    }
+}
---
+++ Újvárosi Katona Gábriel/c#/Zulu/Zulu/Vizsga.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zulu
+{
+    internal class Vizsgak
+    {
+        public int Id { get; set; }
+        public string Vizsgatargy  { get; set; }
+        public string Bizottsag { get; set; }
+        public string Tanarid { get; set; }
+        public int Vizsgazoid { get; set; }
+
+    }
+}
