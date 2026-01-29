092074bc097387eeb88de7901a438581387957fc
Uivarosi Gabriel <gabriel.uivarosi@katonaj-mkszig.hu>
2026-01-29 12:29:30 +0100

    it hurts when i linq

---
+++ Újvárosi Katona Gábriel/c#/Fáj ha linQzok/Fáj ha linQzok/Fáj ha linQzok/Megoldas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fáj_ha_linQzok
+{
+    internal class Megoldas
+    {
+        public int Id { get; set; }
+        public int Feladatid { get; set; }
+        public int Csapatid { get; set; }
+        public DateTime Datum { get; set; }
+        public int Pontszam { get; set; }
+    }
+}
