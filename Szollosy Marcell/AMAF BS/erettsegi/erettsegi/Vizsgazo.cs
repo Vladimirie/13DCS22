871af57ac65d666400c9603f052ffbb929add8f1
Marcell Szollosy <marcell.szollosy@katonaj-mkszig.hu>
2026-01-29 10:59:21 +0100

    erettsegi

---
+++ Szollosy Marcell/AMAF BS/erettsegi/erettsegi/Vizsgazo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace erettsegi
+{
+    public class Vizsgazo
+    {
+        public int Id { get; set; }
+        public string DiakNev { get; set; }
+        public int EvFolyam { get; set; }
+        public string Osztaly { get; set; }
+
+    }
+}
