e831facbbdbe959f2b09fdc95c46a4b21c2e519c
Kiss-Fodor Zsombor <zsombor.kiss-fodor@katonaj-mkszig.hu>
2026-02-05 11:30:41 +0100

    cumposer

---
+++ Kiss-Fodor Zsombor/Backend/testLaravel/testProject/app/Models/OrderItem.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class OrderItem extends Model
+{
+    protected $fillable = [
+        'order_id',
+        'menu_item_id',
+        'quantitiy',
+        'unit_price',
+        'line_total'
+    ];
+
+    protected $casts = [
+        'unit_price' => 'decimal:2',
+        'line_total' => 'decimal:2'
+    ];
+
+    public function menuItem():belongsTo {
+        return $this->belongsTo(MenuItem::class);
+    }
+
+    public function order():belongsTo {
+        return $this->belongsTo(Order::class);
+    }
+}
