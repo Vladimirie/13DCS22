8eeffec0a9bd5d3b2d913d213c1fff97f6dc0415
Gabi455 <gabi455@yahoo.com>
2026-02-16 12:04:50 +0100

    Szőlősi te nem figyelő fasz

--- Újvárosi Katona Gábriel/LARA/SigmaProject2/database/seeders/DatabaseSeeder.php
+++ Újvárosi Katona Gábriel/LARA/SigmaProject2/database/seeders/DatabaseSeeder.php
@@ -16,7 +16,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
+        $this->call([
+            CategorySeeder::class,
+            MenuitemSeeder::class
+        ]);
+        
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
--- Újvárosi Katona Gábriel/LARA/SigmaProject2/routes/api.php
+++ Újvárosi Katona Gábriel/LARA/SigmaProject2/routes/api.php
@@ -1,7 +1,9 @@
 <?php
+
 use App\Http\Controllers\API\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\CategoryController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +12,8 @@ Route::get('/user', function (Request $request) {
 Route::get('/tests',[TestController::class,'index']);
 
 Route::get('/user/{id}', [TestController::class, 'show']);
+
+Route::post('/category', [CategoryController::class, 'show']);
+Route::post('/category/create', [CategoryController::class, 'store']);
+Route::put('/category', [CategoryController::class, 'update']);
+Route::delete('/category', [CategoryController::class, 'destroy']);
