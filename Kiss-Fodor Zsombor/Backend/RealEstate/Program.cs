d37b69298775d4de9850fd68017a01bfeba83dfd
Kiss-Fodor Zsombor <zsombor.kiss-fodor@katonaj-mkszig.hu>
2026-02-26 14:58:14 +0100

    tetves git

---
+++ Kiss-Fodor Zsombor/Backend/RealEstate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace RealEstate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileToRead = "realestates.csv";
+            var hazak = Ad.LoadFromCSV(fileToRead);
+
+            foreach (var item in hazak)
+            {
+                Console.WriteLine(item);
+            }
+
+            //6. feladat
+            var atlagAdat = hazak.Where(a => a.Floors == 0).Average(a => a.Area);
+            Console.WriteLine(atlagAdat.ToString());
+        }
+    }
+}
---
+++ Kiss-Fodor Zsombor/Backend/RealEstate/Seller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RealEstate
+{
+    internal class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+
+        public Seller(int id, string name, string phone)
+        {
+            Id = id;
+            Name = name;
+            Phone = phone;
+        }
+    }
+}
