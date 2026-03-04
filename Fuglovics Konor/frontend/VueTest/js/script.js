b026504a50d5c798d8b6bd9c70f91b87f6c7b04a
Sound Crafter <konor.fuglovics@gmail.com>
2026-03-04 12:50:59 +0100

    more vue SCHET

--- Fuglovics Konor/frontend/VueTest/js/script.js
+++ Fuglovics Konor/frontend/VueTest/js/script.js
@@ -1,37 +1,86 @@
 const {createApp, ref} = Vue;
-const txt = createApp({
+createApp({
 	setup()
 	{
+		const isloaded = ref(false);
+		const sysstatus = ref("");
+		const lastupdated = ref("");
+		const initsystem = () =>
+		{
+			isloaded = true;
+			sysstatus.value = "System online";
+		}
+		const combodata = ref([]);
+		const selectitem = ref("");
+		const fillcombo = () =>
+		{
+			if(!isloaded)
+			{
+				alert("Activate the system first!");
+			}
+			else
+			{
+				const jsonitems =
+				[
+					{id: 1, name: "First option"},
+					{id: 2, name: "Second option"},
+					{id: 3, name: "Third option"}
+				];
+				combodata.value = jsonitems;
+				lastupdated.value = Date.now();
+			}
+		};
 		const textinput = ref('');
-		return{
-			textinput,
-		};
-	}
-});
-txt.mount('#txtapp');
-const counter = createApp({
-	setup()
-	{
 		const count = ref(0);
-		return{
-			count
-		};
-	}
-});
-counter.mount('#cntapp')
-const itemapp = createApp({
-	setup()
-	{
-		const['st', 'nd', 'rd', 'th'];
 		const items = ref(['1st item']);
 		const additem = () =>
 		{
 			items.value.push(`Item #${items.value.length+1}`);
 		};
+		const table = ref(Array(5).fill(null).map(() => Array(5).fill("?")));
+		const fileuploadhandler = (event) =>
+		{
+			const file = event.target.files[0];
+			if(!file)
+			{
+				return;
+			}
+			const read = new FileReader();
+			read.onload = (e) =>
+			{
+				const content = e.target.result;
+				const rows = content.trim().split("\n").map(row => row.split(','));
+				table.value = rows;
+			}
+			read.readAsText(file);
+		}
+		const data = ref(
+		{
+			"Input 1": "",
+			"Input 2": "",
+			"Input 3": "",
+			"Input 4": "",
+			"Input 5": ""
+		});
+		const submitdata = () =>
+		{
+			const msg = Object.entries(data.value).map(([key, value]) => `$(key): $(value)`).join("\n");
+			alert(`All input data:\n\n$(msg)`);
+		}
 		return{
+			initsystem,
+			isloaded,
+			sysstatus,
+			lastupdated,
+			textinput,
+			count,
 			items,
-			additem
+			additem,
+			combodata,
+			selectitem,
+			fillcombo,
+			table,
+			fileuploadhandler
 		};
 	}
-});
-itemapp.mount('#itemapp');
+}).mount('#app');
