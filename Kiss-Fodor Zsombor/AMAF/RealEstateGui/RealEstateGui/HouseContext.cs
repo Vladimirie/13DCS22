f73e42b4cc7e4e0f1b34dc29e51428fb94d13d6b
Kiss-Fodor Zsombor <zsombor.kiss-fodor@katonaj-mkszig.hu>
2026-03-05 14:23:25 +0100

    aaa

---
+++ Kiss-Fodor Zsombor/AMAF/RealEstateGui/RealEstateGui/HouseContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateGui
+{
+    internal class HouseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = "Server=localhost;Database=ingatlan;User=root;Password=;";
+                optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+
+            }
+        }
+        public DbSet<Categories> Categories { get; set; }
+        //public DbSet<RealEstates> RealEstates { get; set; }
+        public DbSet<Seller> Sellers { get; set; }
+    }
+}
