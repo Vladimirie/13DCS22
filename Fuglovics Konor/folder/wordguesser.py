190178939a78780fa11224ef89fb7c6059e5408d
Sound Crafter <konor.fuglovics@gmail.com>
2026-03-13 15:01:42 +0100

    i'm so mad rn

--- Fuglovics Konor/folder/wordguesser.py
+++ Fuglovics Konor/folder/wordguesser.py
@@ -1,20 +1,12 @@
 import random
-import sys
-print(sys.getdefaultencoding())
-a = "ő".encode("utf-8").decode("unicode-escape")
-b = a
-print(a.encode("utf-8"))
-
-
-"""def hunencode(letter):
+import codecs
+'''def hunencode(letter):
 	letter.encode("utf-8")
 	encoded = letter.decode("utf-8")
-	return encoded
-	
+	return encoded'''
+
 wordlist = []
-with open("magyar-szavak.txt", "r") as f:
+with open("magyarszaval.txt", "r", encoding="cp1250") as f:
 	wordlist = [line.strip() for line in f]
-for word in wordlist:
-	word.encode("utf-8")
-	hunencode(word)
-"""
+word = wordlist[random.randint(0,len(wordlist))]
+wordinp = input("")
