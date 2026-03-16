29389eb01662cd1514fdf17bc5c909b8e3933870
Sound Crafter <konor.fuglovics@gmail.com>
2026-03-16 09:50:46 +0100

    awawawawa!

--- Fuglovics Konor/folder/wordguesser.py
+++ Fuglovics Konor/folder/wordguesser.py
@@ -1,12 +1,25 @@
+# -*- coding: cp1250 -*-
 import random
-import codecs
-'''def hunencode(letter):
-	letter.encode("utf-8")
-	encoded = letter.decode("utf-8")
-	return encoded'''
+print('û')
 
-wordlist = []
+"""wordlist = []
+selword = []
+correctchars = 0
+incorrectchars = 0
 with open("magyarszaval.txt", "r", encoding="cp1250") as f:
 	wordlist = [line.strip() for line in f]
 word = wordlist[random.randint(0,len(wordlist))]
+#print(word)
 wordinp = input("")
+for i in word:
+	if i in wordinp:
+		print(i, end="")
+		correctchars += 1
+	else:
+		print("_", end="")
+		incorrectchars += 1
+if len(wordinp) > len(word) or len(wordinp) < len(word) or (len(wordinp) == len(word) and incorrectchars > 0):
+	print("\nHibás szó!")
+print(f"A helyes szó: {word}")"""
+
+#print(f"{len(word)}\n{len(wordinp)}")
---
+++ Fuglovics Konor/frontend/jsthing/js/script.js
@@ -0,0 +1,13 @@
+const (CreateApp, ref) from 'vue';
+CreateApp({
+	const Item =
+	{
+		props: ["name", "price"],
+		template:
+		`
+		<div class="item">
+		
+		</div>
+		`
+	}
+}).mount('#app');
