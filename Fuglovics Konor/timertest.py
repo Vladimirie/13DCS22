8a1ba72bd4186eb097a417364b1383532deb85e2
Sound Crafter <konor.fuglovics@gmail.com>
2026-04-23 13:50:44 +0200

    time

--- Fuglovics Konor/timertest.py
+++ Fuglovics Konor/timertest.py
@@ -5,7 +5,10 @@ def callme(foo):
 
 t = Timer(1, callme, args=["hello"])
 t.start()
+u = Timer(1, callme, args=["huzzah"])
 
 print("Run code immediately here.")
 t.join()
+u.start()
 print("Run code after Timer ends.")
+u.join()
