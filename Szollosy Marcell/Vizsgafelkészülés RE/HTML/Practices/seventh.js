ace63a4782b38d4d46d8c1c0384bb7e249d82a97
unknown <szollosymarci44@gmail.com>
2026-04-27 18:18:03 +0200

    todolist

--- Szollosy Marcell/Vizsgafelkészülés RE/HTML/Practices/seventh.js
+++ Szollosy Marcell/Vizsgafelkészülés RE/HTML/Practices/seventh.js
@@ -1,4 +1,33 @@
-const list = JSON.parse(localStorage.getItem('lists')) || [
-  { Brand: 'Milo', Type: 'Cat', age: '2' },
-  { name: 'Buddy', type: 'Dog', age: '3' }
+const list = JSON.parse(localStorage.getItem('Items')) || [
+  { Brand: 'Axe', Type: 'Bodyspray', Expiration_Date: '2026.11.30' },
+  { Brand: 'HoneyApple', Type: 'Food', Expiration_Date: '2026.10.30' }
 ];
+
+function addItem() {
+  const Brand = document.getElementById('Brand').value;
+  const Type = document.getElementById('Type').value;
+  const Expiration_Date = document.getElementById('Expiration_Date').value;
+  if (!Brand || !Expiration_Date) {
+    alert('Please fill the space');
+    return;
+  }
+  list.push({ Brand, Type, Expiration_Date })
+  SaveItems();
+  window.location.href='list.html';
+}
+
+function SaveItems() {
+  localStorage.setItem('Items', JSON.stringify(list));
+}
+
+function RenderItems() {
+  const lists = document.getElementById('ItemList');
+  if (!list) return;
+
+  list.innerHTML = '';
+  list.forEach(a =>{
+    div.className = 'card';
+    div.innerHTML = '<strong>${a.Brand}</strong><br>Type: ${a.Type}<br>Expiration_Date: ${a.Expiration_Date}';
+    list.appendChild(div);
+  });
+}
